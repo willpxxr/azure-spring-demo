287aebb0d949ef9b27152bcc46aad2ff18b8409e
William Parr <williamparr96@outlook.com>
2020-09-28 15:01:18 +0100

    Update application to have a /api/** endpoint which is secured by IP address

--- src/main/kotlin/com/r3/demo/config/WebSecurityConfig.kt
+++ src/main/kotlin/com/r3/demo/config/WebSecurityConfig.kt
@@ -1,10 +1,12 @@
 package com.r3.demo.config
 
 import com.r3.demo.service.MyUserDetailsService
+import org.apache.juli.logging.LogFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
+import org.springframework.security.config.annotation.web.AbstractRequestMatcherRegistry
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
@@ -20,13 +22,28 @@ class WebSecurityConfig: WebSecurityConfigurerAdapter() {
         return BCryptPasswordEncoder()
     }
 
+    private val whiteListedIps: Set<String> = setOf(
+            "127.0.0.1",
+            "255.1.1.1"
+    )
+
     override fun configure(http: HttpSecurity?) {
+        val ipAddressWhiteListString: String =
+                whiteListedIps.joinToString(separator = " or ") {
+                    "hasIpAddress('$it')"
+                }
+
+        LogFactory.getLog(javaClass).warn(
+                ipAddressWhiteListString
+        )
+
         http!!.authorizeRequests()
                 .antMatchers("/")
                     .permitAll()
+                .antMatchers("/api/**").access(ipAddressWhiteListString)
                 .anyRequest()
                     .authenticated()
-                    .and()
+                .and()
                     .oauth2Login {
                         it.loginPage("/login").permitAll()
                     }
